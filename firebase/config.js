6e7f9edc28989a475c40fee2de04a32befe7c7a2
emanuelcodigos <emanero2000@gmail.com>
2022-02-26 10:44:01 -0300

    first commit

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDMzYMuPSQDabPtz2HUKNSw8Lr5C65uLQA",
+    authDomain: "autoservicio5026-b5751.firebaseapp.com",
+    projectId: "autoservicio5026-b5751",
+    storageBucket: "autoservicio5026-b5751.appspot.com",
+    messagingSenderId: "58961155715",
+    appId: "1:58961155715:web:ecd6927c6ff3fa14566db6"
+  };
+
+firebase.initializeApp(firebaseConfig); 
---
+++ index.js
@@ -0,0 +1,138 @@
+const btnGuardar = document.getElementById('btnGuardar');
+const formulario = document.getElementById('formulario');
+const btnVerDetalles = document.getElementById('btnVerDetalles');
+const divDetalleVentas = document.getElementById('detalleVentas');
+const contendorDetalles = document.getElementById('contenedorTabla');
+const importeTotalVentasHoy = document.getElementById('importeTotalVentasHoy');
+const btnRecargarTabla = document.getElementById('btnRecargarTabla');
+
+const db = firebase.firestore();
+const fecha = new Date;
+
+const docRef = db.collection("carniceria").doc("productos");
+const docRefVentas = db.collection("carniceria").doc("ventas");
+
+docRef.get().then((doc) => {
+    if (doc.exists) {
+
+        const productos = doc.data().productos;
+        let opcionesProducto = '<option selected disabled value="">--Selecciona un corte--</option>';
+        productos.forEach(producto => {
+
+            opcionesProducto += `<option value="${producto}">${producto}</option>`;
+        });
+
+        selectCorte.innerHTML = opcionesProducto;
+
+    } else {
+        console.log("No such document!");
+    }
+}).catch((error) => {
+    console.log("Error getting document:", error);
+});
+
+docRefVentas.get().then((doc) => {
+    if (doc.exists) {
+        const actual = `${fecha.getDate()}-${fecha.getMonth() + 1}-${fecha.getFullYear()}`;
+        ventasHoy = doc.data()[actual];
+        let importe = 0;
+
+        ventasHoy.forEach( venta => {
+            importe += venta.importe;
+        });
+
+        importeTotalVentasHoy.innerHTML = importe;
+
+
+
+    } else {
+        console.log("No such document!");
+    }
+}).catch((error) => {
+    console.log("Error getting document:", error);
+});
+
+
+
+btnGuardar.addEventListener('click', e => {
+    e.preventDefault();
+    guardarVenta();
+
+});
+
+const guardarVenta = () => {
+
+    const producto = formulario.select.value;
+    const precio = parseFloat(formulario.input.value);
+
+    if (producto.trim() === '' || precio.toString().trim() === '') {
+        console.log('Completa todos los campos');
+        return;
+    }
+
+    const actual = `${fecha.getDate()}-${fecha.getMonth() + 1}-${fecha.getFullYear()}`;
+    //console.log(actual);
+
+    const docVenta = {
+        producto,
+        importe: precio
+    }
+
+    try {
+        const ventasRef = db.collection("carniceria").doc("ventas");
+        ventasRef.update({
+            [actual]: firebase.firestore.FieldValue.arrayUnion(docVenta)
+        });
+
+        const importeAcumulado = parseFloat(importeTotalVentasHoy.innerHTML);
+        
+        importeTotalVentasHoy.innerHTML = importeAcumulado + precio;
+        alert('Guardado correctamente');
+        formulario.reset();
+    } catch (error) {
+        alert(`Ocurrio un error: ${error.message}`)
+    }
+
+
+
+
+}
+
+
+btnVerDetalles.addEventListener('click', () => {
+
+    contendorDetalles.style.display = 'block';
+    mostrarTodasLasVentasDelDia();
+});
+
+btnRecargarTabla.addEventListener('click', () => {
+    mostrarTodasLasVentasDelDia();
+});
+
+const mostrarTodasLasVentasDelDia = () => {
+    const actual = `${fecha.getDate()}-${fecha.getMonth() + 1}-${fecha.getFullYear()}`;
+    console.log(actual);
+
+    const docRef = db.collection("carniceria").doc("ventas");
+
+    docRef.get().then((doc) => {
+        if (doc.exists) {
+
+            const ventasDelDia = doc.data()[actual];
+            let detalles = "";
+            let importe = 0;
+            ventasDelDia.forEach(venta => {
+                importe += venta.importe;
+                detalles += `<tr><td>${venta.producto}</td><td>$ ${venta.importe}</td></tr>`;
+            });
+
+            divDetalleVentas.innerHTML = detalles;
+            importeTotalVentasHoy.innerHTML = importe;
+
+        } else {
+            console.log("No such document!");
+        }
+    }).catch((error) => {
+        console.log("Error getting document:", error);
+    });
+}
