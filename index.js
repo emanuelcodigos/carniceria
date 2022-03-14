dca96e65ae43e68294ef7ef5464ce3702a2e15c9
emanuelcodigos <emanero2000@gmail.com>
2022-03-14 19:44:34 -0300

    todos los metodos de firebase modificado, por cambios en la database

--- index.js
+++ index.js
@@ -6,13 +6,25 @@ const contendorDetalles = document.getElementById('contenedorTabla');
 const importeTotalVentasHoy = document.getElementById('importeTotalVentasHoy');
 const btnRecargarTabla = document.getElementById('btnRecargarTabla');
 const tablaVentasAnteriores = document.getElementById('detalleVentasAnteriores');
+const divFiltrar = document.getElementById('divFiltrar');
+
 
 const db = firebase.firestore();
 const fecha = new Date;
 
 const docRef = db.collection("carniceria").doc("productos");
-const docRefVentas = db.collection("carniceria").doc("ventas");
-const docRefVentasAnteriores = db.collection("carniceria").doc("ventas"); 
+const docRefVentas = db.collection("carniceria").doc("ventasNuevo");
+
+
+const MESES = [
+    { id: 1, mes: 'enero' }, { id: 2, mes: 'febrero' }, { id: 3, mes: 'marzo' },
+    { id: 4, mes: 'abril' }, { id: 5, mes: 'mayo' }, { id: 6, mes: 'junio' },
+    { id: 7, mes: 'julio' }, { id: 8, mes: 'agosto' }, { id: 9, mes: 'septiembre' },
+    { id: 10, mes: 'octubre' }, { id: 11, mes: 'noviembre' }, { id: 12, mes: 'diciembre' }
+];
+
+const DIA_ACTUAL = fecha.getDate().toString();
+const MES_ACTUAL = MESES[fecha.getMonth()].mes;
 
 docRef.get().then((doc) => {
     if (doc.exists) {
@@ -33,66 +45,102 @@ docRef.get().then((doc) => {
     console.log("Error getting document:", error);
 });
 
-docRefVentas.get().then((doc) => {
+
+
+docRefVentas.collection(MES_ACTUAL).doc(DIA_ACTUAL).get().then((doc) => {
     if (doc.exists) {
-        const actual = `${fecha.getDate()}-${fecha.getMonth() + 1}-${fecha.getFullYear()}`;
-        ventasHoy = doc.data()[actual];
+        
+        ventasHoy = doc.data();
         let importe = 0;
 
-        ventasHoy.forEach( venta => {
+        ventasHoy.ventas.forEach(venta => {
             importe += venta.importe;
         });
 
         importeTotalVentasHoy.innerHTML = importe.toFixed(2);
 
-
-
     } else {
-        console.log("No such document!");
+        importeTotalVentasHoy.innerHTML = 0.00;
     }
 }).catch((error) => {
     console.log("Error getting document:", error);
 });
 
 
-docRefVentasAnteriores.get().then((doc) => {
-    if (doc.exists) {
-        
-        const ventas = doc.data();
+const crearDivFiltrar = () => {
 
-        //console.log(ventas);
+    let opcionesMeses = '';
+    let opcionesDias = '';
+    MESES.forEach(mes => {
+        opcionesMeses += `<option value="${mes.id}">${mes.mes}</option>`;
+    });
 
-        const arrayVentas = Object.entries(ventas);
+    for (let i = 1; i <= 31; i++) {
+        opcionesDias += `<option value="${i}">${i}</option>`
+    }
+
+    divFiltrar.innerHTML = `
+    <div class="divItemForm">
+    <label>Mes</label>
+    <select name="selectMes">${opcionesMeses}</select>
+    </div>
+
+    <div class="divItemForm">
+    <label>Dia</label>
+    <select name="selectDia">${opcionesDias}</select>
+    </div>
+
+    <div class="divItemForm">
+    <label class="textoOculto">..</label>
+      <button class="btnFiltrar" onClick="filtrarVentasAnteriores()">Filtrar</button>
+    </div>
+    
+    `;
+}
+
+crearDivFiltrar();
+
+
+const filtrarVentasAnteriores = () => {
+
+    const selectMes = divFiltrar.children[0].children[1].value;
+    const selectDia = divFiltrar.children[1].children[1].value;
+    const diaActual = fecha.getDate();
+
+    try {
         let htmlTabla = '';
-        arrayVentas.forEach( ventasDia => {
-            
-            let importeVentas = 0;
-            ventasDia[1].forEach( venta => {
-                importeVentas += venta.importe;
+        const docRef = db.collection('carniceria').doc('ventasNuevo').
+            collection(MESES[selectMes - 1].mes);
+        docRef.where('fecha', '>=', parseInt(selectDia)).where('fecha', '<=', diaActual).get()
+            .then(querySnapshot => {
+
+                querySnapshot.forEach((doc) => {
+
+                    const data = doc.data();
+
+                    let importeTotal = 0;
+                    data.ventas.forEach(venta => {
+                        importeTotal += venta.importe;
+                    });
+
+                    htmlTabla += `<tr><td>${data.fecha}/${selectMes}</td><td>$ ${importeTotal.toFixed(2)}</td></tr>`;
+                });
+
+                if (htmlTabla === '') {
+                    htmlTabla = '<div class="alertError">No hay registros para estas fechas..</div>'
+                }
+                tablaVentasAnteriores.innerHTML = htmlTabla;
+
             });
 
-            htmlTabla += `<tr><td>${ventasDia[0]}</td><td>$ ${importeVentas.toFixed(2)}</td></tr>`;
-            
-        });
 
-        tablaVentasAnteriores.innerHTML = htmlTabla;
-        
 
-        //importeTotalVentasHoy.innerHTML = importe.toFixed(2);
-    } else {
-        console.log("No such document!");
+    } catch (error) {
+        console.log(error.message);
     }
-}).catch((error) => {
-    console.log("Error getting document:", error);
-});
 
-
-btnGuardar.addEventListener('click', e => {
-    e.preventDefault();
-    guardarVenta();
-
-});
-
+}
+/*
 const guardarVenta = () => {
 
     const producto = formulario.select.value;
@@ -101,14 +149,13 @@ const guardarVenta = () => {
     if (producto.trim() === '' || precio.trim() === '') {
         return alert('Completa todos los campos');
     }
-    if( parseFloat(precio) > 100000 || precio === NaN){
+    if (parseFloat(precio) > 100000 || precio === NaN) {
         return alert('Ingresa un precio valido.');
     }
 
-    console.log(precio);
-
-    const actual = `${fecha.getDate()}-${fecha.getMonth() + 1}-${fecha.getFullYear()}`;
     
+    const actual = `${fecha.getDate()}-${fecha.getMonth() + 1}-${fecha.getFullYear()}`;
+
     const docVenta = {
         producto,
         importe: parseFloat(precio)
@@ -121,7 +168,7 @@ const guardarVenta = () => {
         });
 
         const importeAcumulado = parseFloat(importeTotalVentasHoy.innerHTML);
-        
+
         importeTotalVentasHoy.innerHTML = (importeAcumulado + parseFloat(precio)).toFixed(2);
         alert('Guardado correctamente');
         formulario.reset();
@@ -133,7 +180,7 @@ const guardarVenta = () => {
 
 
 }
-
+*/
 
 btnVerDetalles.addEventListener('click', () => {
 
@@ -145,30 +192,103 @@ btnRecargarTabla.addEventListener('click', () => {
     mostrarTodasLasVentasDelDia();
 });
 
+
 const mostrarTodasLasVentasDelDia = () => {
-    const actual = `${fecha.getDate()}-${fecha.getMonth() + 1}-${fecha.getFullYear()}`;
-    //console.log(actual);
 
-    const docRef = db.collection("carniceria").doc("ventas");
+    const diaActual = fecha.getDate().toString();
+    const mesActual = MESES[fecha.getMonth()].mes;
 
-    docRef.get().then((doc) => {
-        if (doc.exists) {
+    const docRef = db.collection("carniceria").doc("ventasNuevo").collection(mesActual).doc(diaActual);
 
-            const ventasDelDia = doc.data()[actual];
-            let detalles = "";
-            let importe = 0;
-            ventasDelDia.forEach(venta => {
-                importe += venta.importe;
-                detalles += `<tr><td>${venta.producto}</td><td>$ ${venta.importe}</td></tr>`;
+    try {
+
+        docRef.get()
+            .then(doc => {
+                if (doc.exists) {
+
+                    const ventasDelDia = doc.data();
+
+                    let detalles = "";
+                    let importe = 0;
+
+                    ventasDelDia.ventas.forEach(venta => {
+                        importe += venta.importe;
+                        detalles += `<tr><td>${venta.producto}</td><td>$ ${venta.importe}</td></tr>`;
+                    });
+
+                    divDetalleVentas.innerHTML = detalles;
+                    importeTotalVentasHoy.innerHTML = importe.toFixed(2);
+                }
             });
+    } catch (error) {
+        console.log(error.message);
+    }
 
-            divDetalleVentas.innerHTML = detalles;
-            importeTotalVentasHoy.innerHTML = importe.toFixed(2);
+}
 
-        } else {
-            console.log("No such document!");
-        }
-    }).catch((error) => {
-        console.log("Error getting document:", error);
-    });
+
+const guardarNuevaVenta = async () => {
+
+    const producto = formulario.select.value;
+    const precio = formulario.input.value;
+
+    if (producto.trim() === '' || precio.trim() === '') {
+        return alert('Completa todos los campos');
+    }
+    if (parseFloat(precio) > 100000 || precio === NaN) {
+        return alert('Ingresa un precio valido.');
+    }
+
+    const fechaActual = { dia: fecha.getDate().toString(), mes: MESES[fecha.getMonth()] };
+
+    const docVenta = {
+        producto,
+        importe: parseFloat(precio)
+    }
+
+    try {
+
+        const ventasRef = db.collection("carniceria").doc("ventasNuevo").
+            collection(fechaActual.mes.mes).doc(fechaActual.dia);
+
+        ventasRef.update({
+            ventas: firebase.firestore.FieldValue.arrayUnion(docVenta)
+        });
+
+        const importeAcumulado = parseFloat(importeTotalVentasHoy.innerHTML);
+
+        importeTotalVentasHoy.innerHTML = (importeAcumulado + parseFloat(precio)).toFixed(2);
+        alert('Guardado correctamente');
+        formulario.reset();
+
+    } catch (error) {
+        alert(`Ocurrio un error: ${error.message}`)
+    }
+
+}
+
+btnGuardar.addEventListener('click', e => {
+    e.preventDefault();
+    guardarNuevaVenta();
+
+});
+
+
+
+//CREAR COLECCION Y DOCUMENTOS POR MES
+const crearDocumentos = (dia) => {
+
+    const fechaAct = {
+        numero: `${dia}`,
+        mes: 'abril'
+    }
+    const docRef = db.collection('carniceria').doc('ventasNuevo').collection(fechaAct.mes).doc(fechaAct.numero);
+
+    const data = {
+        descripcion: `Ventas del dia ${fechaAct.numero}/${fechaAct.mes}/2022`,
+        ventas: []
+    }
+
+    docRef.set(data);
+
 }
